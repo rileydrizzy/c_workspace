6de395c9cd1f55e74ae058bf4e5f4a25aee56316
Ladipo Ezekiel Ipadeola <rileydrizzy@hotmail.com>
2024-11-14 05:05:33 +0000

    updates

--- practice/prac.c
+++ practice/prac.c
@@ -1,3 +1,18 @@
 #include <stdio.h>
-#include <stdint.h>
+#include <stdlib.h>
+void swap(int *A, int *B)
+{
+    int temp = *A;
+    *A = *B;
+    *B = temp;
+}
 
+int main()
+{
+    int number_1 = 22;
+    int number2 = 44;
+    printf("Before swap: Number_1 = %i and Number_2 = %i \n", number_1, number2);
+    swap(&number_1, &number2);
+    printf("After swap: Number_1 = %i and Number_2 = %i \n", number_1, number2);
+    return EXIT_SUCCESS;
+}
