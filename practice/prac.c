df1caf37c9064e2a596e6bd65011f8beaa6772aa
Ladipo Ezekiel Ipadeola <rileydrizzy@hotmail.com>
2024-11-16 06:29:08 +0000

    updates

--- practice/prac.c
+++ practice/prac.c
@@ -5,6 +5,22 @@
 int main()
 {
     char line[80];
-    FILE *in = fopen("spooky.csv", );
-    FILE *file1 = fopen
+    FILE *in = fopen("spooky.csv", "r");
+    FILE *file1 = fopen("ufos.csv", "w");
+    FILE *file2 = fopen("disappearances.csv", "w");
+    FILE *file3 = fopen("others.csv", "w");
+    while (fscanf(in, "%79[^\n],\n", line) == 1)
+    {
+        if (strstr(line, "UFO"))
+            fprintf(file1, "%s\n", line);
+        else if (strstr(line, "Disappearance"))
+            fprintf(file2, "%s\n", line);
+        else
+            fprintf(file3, "%s\n", line);
+    }
+    fclose(file1);
+    fclose(file2);
+    fclose(file3);
+
+    return EXIT_SUCCESS;
 }
