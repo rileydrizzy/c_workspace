cdd3ee8c510844e51747e11b2677af18b597bb4c
Ladipo Ezekiel Ipadeola <rileydrizzy@hotmail.com>
2024-12-18 00:37:14 +0000

    updates

---
+++ practice/ass_test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Obj dumbed, reserver eng //
+
+int main(void)
+{
+    int X = 1337;
+    while (X--)
+    {
+        printf("ayo\n");
+    }
+    return EXIT_SUCCESS;
+}
