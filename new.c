7d037b35186ce7d22bf85ffdd96610a3644a0ded
Ladipo Ezekiel Ipadeola <rileydrizzy@hotmail.com>
2025-02-06 02:20:19 +0000

    updates

---
+++ new.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+// inline comnets 
+
+/*Doc comnets 
+
+span long pages 
+
+*/
+
+int main()
+{
+    char name[5] = "ladi";
+    printf("%s \n", name);
+    return EXIT_SUCCESS;
+}
