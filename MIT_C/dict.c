edba8c87f29fa4e7703f9c0c06d746c8aee26d26
Ladipo Ezekiel Ipadeola <105759894+rileydrizzy@users.noreply.github.com>
2025-06-10 03:27:05 +0000

    updates

---
+++ MIT_C/dict.c
@@ -0,0 +1,10 @@
+#include "dict.h"
+// #include
+
+/* data structure for the dictionary */
+
+char *the_dictionary[1000];
+
+void load_dictionary() {}
+
+char * lookup(char name[]) {}
--- MIT_C/prob1.c
+++ MIT_C/prob1.c
@@ -2,21 +2,30 @@
 #include <stdio.h>
 #include <time.h>
 
+/*
+Without setting a of the variables a,b,c and n, and also using no optimization in the complier,
+it took an average of about 6.5msec for the computation (Ten million times)to be completed.
+By making the variable a,b,c a register variables, it took an average of about 6.45msec for the computation to be completed.
+it was also observed that making the n, a register variable also, resulted to an average of about 6.4msec.
+
+*/
+
 #define NMAX 25
 static unsigned int results_buffer[NMAX];
 
 void fibonacci()
 {
 	/* here are the variables to set as registers */
-	unsigned int a = 0;
-	unsigned int b = 1;
-	unsigned int c;
-	int n;
+	register unsigned int a = 0;
+	register unsigned int b = 1;
+	register unsigned int c;
+	register int n;
 
 	/* do not edit below this line */
 	results_buffer[0] = a;
 	results_buffer[1] = b;
-	for (n = 2; n < NMAX; n++) {
+	for (n = 2; n < NMAX; n++)
+	{
 		c = a + b;
 		results_buffer[n] = c; /* store code in results buffer */
 		a = b;
@@ -24,15 +33,16 @@ void fibonacci()
 	}
 }
 
-int main(void) {
-	
+int main(void)
+{
+
 	int n, ntests = 10000000;
 	clock_t tstart, tend;
 	double favg;
 
 	/* do profiling */
 	tstart = clock();
-	
+
 	for (n = 0; n < ntests; n++)
 		fibonacci();
 
@@ -40,9 +50,9 @@ int main(void) {
 	/* end profiling */
 
 	/* compute average execution time */
-	favg = ((double)(tend - tstart))/CLOCKS_PER_SEC/ntests;
+	favg = ((double)(tend - tstart)) / CLOCKS_PER_SEC / ntests;
 
 	/* print avg execution time in milliseconds */
-	printf("Avg. execution time: %g msec\n",favg*1000);
+	printf("Avg. execution time: %g msec\n", favg * 1000);
 	return 0;
 }
