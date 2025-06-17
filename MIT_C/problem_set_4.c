77c4bfff3dca015cba58cce9d994aabbbf8204ca
Ladipo Ezekiel Ipadeola <105759894+rileydrizzy@users.noreply.github.com>
2025-06-17 15:52:23 +0000

    updates

--- MIT_C/problem_set_4.c
+++ MIT_C/problem_set_4.c
@@ -1,8 +1,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void shift_element(unsigned int i, int *arr)
+{
+    int ivalue;
+    for (ivalue = arr[i]; i && arr[i - 1] > ivalue; i--)
+        arr[i] = arr[i - 1];
+    arr[i] = ivalue;
+}
 
+void insertion_sort(int *arr)
+{
+    unsigned int i, len = 10;
+    for (i = 1; i < len; i++)
+        ;
+    if (arr[i] < arr[i - 1])
+        shift_element(i, arr);
+}
 
-void shift_element(unsigned int i){
-    int ivalue
+int main(void)
+{
+    int arr[] = {10, 7, 2, 34, 6, 89, 1, 212, 8, 12, 66};
+    insertion_sort(arr);
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return EXIT_SUCCESS;
 }
