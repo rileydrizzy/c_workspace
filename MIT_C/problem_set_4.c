23ab9cfccef275fb399733a503d9a4084ea39417
Ladipo Ezekiel Ipadeola <105759894+rileydrizzy@users.noreply.github.com>
2025-06-18 02:05:23 +0000

    updates

--- MIT_C/problem_set_4.c
+++ MIT_C/problem_set_4.c
@@ -1,31 +1,54 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void shift_element(unsigned int i, int *arr)
+int array_int[] = {10, 7, 2, 34, 6, 89, 1, 212, 8, 12, 66};
+
+void shift_element(unsigned int i)
 {
     int ivalue;
-    for (ivalue = arr[i]; i && arr[i - 1] > ivalue; i--)
-        arr[i] = arr[i - 1];
-    arr[i] = ivalue;
+    for (ivalue = array_int[i]; i && array_int[i - 1] > ivalue; i--)
+        array_int[i] = array_int[i - 1];
+    array_int[i] = ivalue;
 }
 
-void insertion_sort(int *arr)
+void insertion_sort(void)
 {
-    unsigned int i, len = 10;
+    unsigned int i, len = 11;
     for (i = 1; i < len; i++)
-        ;
-    if (arr[i] < arr[i - 1])
-        shift_element(i, arr);
+    {
+        if (array_int[i] < array_int[i - 1])
+            shift_element(i);
+    }
+}
+
+void print_out_arr()
+{
+    for (int i = 0; i < 11; i++)
+    {
+        printf("%d ", array_int[i]);
+    }
+    printf("\n");
+}
+
+void shift_element_pointer(int *pElement)
+{
+    int value;
+}
+
+void insetion_sort_pointer(int *arr_pointer)
+{
+    int *arr_unsorted_start, arr_end_pointer = arr_pointer + 10;
+    for (arr_unsorted_start = arr_pointer + 1; arr_unsorted_start < arr_end_pointer; arr_unsorted_start++)
+    {
+        if (*arr_unsorted_start < *(arr_unsorted_start - 1))
+            shift_element_pointer(arr_unsorted_start);
+    }
 }
 
 int main(void)
 {
-    int arr[] = {10, 7, 2, 34, 6, 89, 1, 212, 8, 12, 66};
-    insertion_sort(arr);
-    for (int i = 0; i < 10; i++)
-    {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
+    // insertion_sort();
+    insetion_sort_pointer(&array_int);
+    print_out_arr();
     return EXIT_SUCCESS;
 }
